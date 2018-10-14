fdc71703937c018a447914f28963e94deaf6baf5
Tiffany Bennett <tiffany@stormbit.net>
2018-10-13 21:45:37 -0700

    Fix performance issues with tooltip view (Fixes #39)

--- src/Components/TextLabel.lua
+++ src/Components/TextLabel.lua
@@ -5,6 +5,23 @@ local Roact = require(Modules.Roact)
 local Constants = require(Modules.Plugin.Constants)
 
 local function TextLabel(props)
+	local update
+
+	if props.TextWrapped then
+		function update(rbx)
+			local width = rbx.AbsoluteSize.x
+			local tb = TextService:GetTextSize(rbx.Text, rbx.TextSize, rbx.Font, Vector2.new(width - 2, 100000))
+			rbx.Size = UDim2.new(1, 0, 0, tb.y)
+		end
+	else
+		function update(rbx)
+			local tb = rbx.TextBounds
+			rbx.Size = UDim2.new(props.Width or UDim.new(0, tb.x), UDim.new(0, tb.y))
+		end
+	end
+
+	local autoSize = not props.Size
+
 	return Roact.createElement("TextLabel", {
 		BackgroundTransparency = 1.0,
 		Font = props.Font or Enum.Font.SourceSans,
@@ -18,34 +35,9 @@ local function TextLabel(props)
 		TextXAlignment = props.TextXAlignment or Enum.TextXAlignment.Left,
 		TextYAlignment = props.TextYAlignment,
 
-		[Roact.Ref] = not props.Size and function(rbx)
-			if not rbx then return end
-
-			if props.TextWrapped then
-				local function update()
-					local width = rbx.AbsoluteSize.x
-					local tb = TextService:GetTextSize(rbx.Text, rbx.TextSize, rbx.Font, Vector2.new(width - 2, 100000))
-					rbx.Size = UDim2.new(1, 0, 0, tb.y)
-				end
-				rbx:GetPropertyChangedSignal("TextBounds"):Connect(update)
-				local oldX = rbx.AbsoluteSize.x
-				rbx:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
-					if oldX ~= rbx.AbsoluteSize.x then
-						oldX = rbx.AbsoluteSize.x
-						update()
-					end
-				end)
-				rbx:GetPropertyChangedSignal("Parent"):Connect(update)
-				update()
-			else
-				local function update()
-					local tb = rbx.TextBounds
-					rbx.Size = UDim2.new(props.Width or UDim.new(0, tb.x), UDim.new(0, tb.y))
-				end
-				rbx:GetPropertyChangedSignal("TextBounds"):Connect(update)
-				update()
-			end
-		end or nil,
+		[Roact.Change.TextBounds] = autoSize and update or nil,
+		[Roact.Change.AbsoluteSize] = autoSize and update or nil,
+		[Roact.Change.Parent] = autoSize and update or nil,
 	})
 end
 
