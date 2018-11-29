ead03f8ab87dad8810fefbea2f3ab161e15ccd0a
AmaranthineCodices <git@amaranthinecodices.me>
2018-11-28 20:39:21 -0500

    studio theme page

--- src/Components/Page.lua
+++ src/Components/Page.lua
@@ -1,61 +1,71 @@
 local Modules = script.Parent.Parent.Parent
 local Roact = require(Modules.Roact)
+local e = Roact.createElement
 
-local ThemeAccessor = require(script.Parent.ThemeAccessor)
+local StudioThemeAccessor = require(script.Parent.StudioThemeAccessor)
 local Icon = require(script.Parent.Icon)
 local TextLabel = require(script.Parent.TextLabel)
 
 local function Page(props)
-	return ThemeAccessor.withTheme(function(theme)
-		return Roact.createElement("ImageButton", {
+	return StudioThemeAccessor.withTheme(function(theme)
+		return e("ImageButton", {
 			Size = UDim2.new(1, 0, 1, 0),
-			BackgroundColor3 = theme:get("MainSection", "BackgroundColor3"),
+			BackgroundColor3 = theme:GetColor("MainBackground"),
 			ZIndex = 10,
 			Visible = props.visible,
 			AutoButtonColor = false,
 		}, {
-			Topbar = Roact.createElement("Frame", {
+			Topbar = e("Frame", {
 				Size = UDim2.new(1, 0, 0, 32),
-				BackgroundColor3 = theme:get("Topbar", "BackgroundColor3"),
+				BackgroundColor3 = theme:GetColor("Titlebar"),
 				BorderSizePixel = 0,
 				ZIndex = 2,
 			}, {
-				Back = Roact.createElement("TextButton", {
+				Back = e("TextButton", {
 					Size = UDim2.new(0, 48, 0, 32),
 					Text = "Back",
 					TextSize = 20,
 					Font = Enum.Font.SourceSansBold,
 					BackgroundTransparency = 1.0,
-					TextColor3 = theme:get("Topbar", "TextColor3"),
+					TextColor3 = theme:GetColor("TitlebarText"),
 
 					[Roact.Event.MouseButton1Click] = function(rbx)
 						props.close()
 					end,
 				}),
-				Title = Roact.createElement("Frame", {
+				Title = e("Frame", {
 					Size = UDim2.new(1, 0, 1, 0),
 					BackgroundTransparency = 1.0,
 				}, {
-					UIListLayout = Roact.createElement("UIListLayout", {
+					UIListLayout = e("UIListLayout", {
 						HorizontalAlignment = Enum.HorizontalAlignment.Center,
 						VerticalAlignment = Enum.VerticalAlignment.Center,
 						FillDirection = Enum.FillDirection.Horizontal,
 						SortOrder = Enum.SortOrder.LayoutOrder,
 						Padding = UDim.new(0, 4),
 					}),
-					Icon = props.titleIcon and Roact.createElement(Icon, {
+					Icon = props.titleIcon and e(Icon, {
 						Name = props.titleIcon,
 						LayoutOrder = 1,
 					}),
-					Label = Roact.createElement(TextLabel, {
+					Label = e(TextLabel, {
 						Text = props.title,
 						LayoutOrder = 2,
-						TextColor3 = theme:get("Topbar", "TextColor3"),
+						TextColor3 = theme:GetColor("TitlebarText"),
 						Font = Enum.Font.SourceSansSemibold,
 					}),
+				}),
+				Separator = e("Frame", {
+					-- This separator acts as a bottom border, so we should use the border color, not the separator color
+					BackgroundColor3 = theme:GetColor("Border"),
+					BorderSizePixel = 0,
+					Size = UDim2.new(1, 0, 0, 1),
+					Position = UDim2.new(0, 0, 1, 0),
+					AnchorPoint = Vector2.new(0, 1),
+					ZIndex = 2,
 				})
 			}),
-			Body = Roact.createElement("Frame", {
+			Body = e("Frame", {
 				Size = UDim2.new(1, 0, 1, -32),
 				Position = UDim2.new(0, 0, 0, 32),
 				BackgroundTransparency = 1.0,
