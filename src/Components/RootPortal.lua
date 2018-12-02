2e975a2b2bb6ed1e089701417bde68e7c25d4edc
AmaranthineCodices <git@amaranthinecodices.me>
2018-12-02 15:20:07 -0500

    fix RootPortal

--- src/Components/RootPortal.lua
+++ src/Components/RootPortal.lua
@@ -1,3 +1,16 @@
+--[[
+    RootPortal allows rendering elements into the root of the tag editor's UI tree safely.
+    Using portals alone would cause an error because Roact doesn't update refs until after
+    children have been mounted, and portals cannot have a target of nil. For example, this
+    code would break on initial render (but work for all subsequent renders):
+
+    render()
+        return e(Roact.Portal, {
+            target = self._context[rootKey].current
+        })
+    end
+]]
+
 local Modules = script.Parent.Parent.Parent
 local Roact = require(Modules.Roact)
 local e = Roact.createElement
@@ -21,7 +34,9 @@ function RootPortal:render()
 end
 
 function RootPortal:didMount()
-    self:setState({})
+    spawn(function()
+        self:setState({})
+    end)
 end
 
 return RootPortal
