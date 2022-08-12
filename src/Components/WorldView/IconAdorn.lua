1851b8ba4fe78ec8093b8a31274de93ee18738d0
Tiffany Bennett <tiffany@tiffnix.com>
2022-08-11 19:10:38 -0700

    Re-add multi-icon rendering

--- src/Components/WorldView/IconAdorn.lua
+++ src/Components/WorldView/IconAdorn.lua
@@ -2,28 +2,31 @@ local Modules = script.Parent.Parent.Parent.Parent
 local Roact = require(Modules.Roact)
 local Icon = require(Modules.Plugin.Components.Icon)
 
+local SCALE_ONE = 1.5
+local SCALE_GRID = 2.0
+
 local function IconAdorn(props)
 	local children = {}
-	if #props.Icon > 1 then
-		children.UIListLayout = Roact.createElement("UIListLayout", {
-			FillDirection = Enum.FillDirection.Horizontal,
-			Padding = UDim.new(2 / 16, 0),
-		})
-	end
-	for i = 1, #props.Icon do
+	local count = #props.Icon
+
+	local gridSize = math.ceil(math.sqrt(count))
+	local width = if count > 1 then SCALE_GRID else SCALE_ONE
+
+	for i = 1, count do
+		local x = (i - 1) % gridSize
+		local y = math.floor((i - 1) / gridSize)
 		local icon = props.Icon[i]
 		children[i] = Roact.createElement(Icon, {
 			Name = icon,
-			Size = UDim2.new(1 / #props.Icon, 0, 1, 0),
+			Position = UDim2.fromScale(x / gridSize, y / gridSize),
+			Size = UDim2.fromScale(1 / gridSize, 1 / gridSize),
 			TextScaled = true,
 		})
 	end
 	return Roact.createElement("BillboardGui", {
 		Adornee = props.Adornee,
-        Size = UDim2.fromScale(1.5, 1.5),
-        SizeOffset = Vector2.new(0.5, 0.5),
-        StudsOffset = Vector3.new(-0.75, -0.75, 0.0),
-        AlwaysOnTop = props.AlwaysOnTop,
+		Size = UDim2.fromScale(width, width),
+		AlwaysOnTop = props.AlwaysOnTop,
 	}, children)
 end
 
