3c8b552a9b25750707ec3d0989039ac176e3e742
Vexot <69873276+Vexot@users.noreply.github.com>
2022-08-22 23:24:23 -0500

    Update Spanish Translations + Fallback Translator

--- src/tr.lua
+++ src/tr.lua
@@ -8,17 +8,27 @@ local supportedLanguages = {
 	["es_ES"] = "es-es"
 }
 
-local function tr(key: string, args: any): string
-	local locale = supportedLanguages[Studio.StudioLocaleId] or "en-us"
-	local l18n = Localization:GetTranslator(locale)
+local defaultLocale = "en-us"
+local locale = supportedLanguages[Studio.StudioLocaleId]
 
+local translator = Localization:GetTranslator(locale or defaultLocale)
+local fallback = locale ~= defaultLocale and Localization:GetTranslator(defaultLocale) -- JIC the string hasn't been translated
+
+local function tr(key: string, args: any): string
 	if Config.testLocalization then
 		return key
 	end
 
 	local ok, result = pcall(function()
-		return l18n:FormatByKey(key, args)
+		return translator:FormatByKey(key, args)
 	end)
+
+	if not ok and fallback then
+		ok, result = pcall(function()
+			return fallback:FormatByKey(key, args)
+		end)
+	end
+
 	if ok then
 		return result
 	else
